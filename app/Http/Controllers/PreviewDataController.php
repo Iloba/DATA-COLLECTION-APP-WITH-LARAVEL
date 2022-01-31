2ff6040685ff9e277ee8dacdf7b935fd22935a1d
Iloba <scarlettsney@gmail.com>
2022-01-31 11:37:39 +0100

    update

--- app/Http/Controllers/PreviewDataController.php
+++ app/Http/Controllers/PreviewDataController.php
@@ -15,15 +15,17 @@ use App\Notifications\SubmissionNotification;
 class PreviewDataController extends Controller
 {
     //preview data
-    public function preview(User $user){
+    public function preview(User $user)
+    {
         // dd($user);
 
-        return view('user.preview_data',[
+        return view('user.preview_data', [
             'user' => $user
         ]);
     }
 
-    public function submit(Request $request, User $user){
+    public function submit(Request $request, User $user)
+    {
         // dump($user);
 
         //validate form request
@@ -36,63 +38,60 @@ class PreviewDataController extends Controller
         ];
 
 
-       $validator =  Validator::make($request->all(), $rules, $messages);
+        $validator =  Validator::make($request->all(), $rules, $messages);
 
-       if($validator->fails()){
-        return redirect()->back()
-        ->withErrors($validator)
-        ->withInput();
-       };
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        };
 
 
-        
+
         $user = User::find($user->id);
+        // dd($user->email);
 
-       //Check if email is verified
+        //Check if email is verified
         // if(!$user->email_verified == true){
         //     return redirect()->back()->with('error', 'Please Verify your Email before submitting');
         // }
 
 
 
-       //generate pdf and send to user email
+        //generate pdf and send to user email
         // $pdf = PDF::loadView('user.pdf-page');
 
         //  $pdf->download('user-pdf');
 
-         //Try and catch syntaxxx to send error (Never forget)
-         try {
+        //Try and catch syntaxxx to send error (Never forget)
+        try {
+
             //Send Verification mail after registration
-           Mail::to($user->email)->send(new SubmissionNotification($user->username));
+            $user->notify(new SubmissionNotification($user->name));
 
-           //redirect user to email verification page 
-           return redirect()->route('email-verification')->with('success', 'Congratulations you just submitted your form');
+           
+            
+        } catch (Exception $error) {
+            return back()->with('error', 'Registration complete but Something went wrong; Could not send verification mail');
+        }
 
-       } catch (Exception $error){
-           return back()->with('error', 'Registration complete but Something went wrong; Could not send verification mail');
-       }
-      
 
 
         //submit form
-       $user->submitted = true;
+        $user->submitted = true;
 
-       $user->save();
-
-
-       //Destroy session........(Logout user)
-       Session::forget('user');
+        $user->save();
 
 
-       //redirect to submission page
-       return redirect()->route('form-submitted')->with('success', 'Congratulations, your Form submitted successfully, Please check your email for confirmation');
-       
-        
-      
+        //Destroy session........(Logout user)
+        Session::forget('user');
 
+
+        //redirect to submission page
+        return redirect()->route('form-submitted')->with('success', 'Congratulations, your Form submitted successfully, Please check your email for confirmation');
     }
-   
-      //generate pdf and download
+
+    //generate pdf and download
     // public function downloadPDF(){
     //      $pdf = PDF::loadView('user.pdf-page');
 
@@ -100,9 +99,8 @@ class PreviewDataController extends Controller
     // }
 
 
-    
-    
-    
 
-   
+
+
+
 }
